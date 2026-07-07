83e01258971f9627e85e6bacfede1e51ca156ac0
minenhlemagangane-web <minenhlemagangane@gmail.com>
2026-07-07 09:14:32 +0200

    Delete Retail Sales SQL Workbook.py

--- Retail Sales SQL Workbook.py
+++
@@ -1,8 +0,0 @@
-# Databricks notebook source
-# /// script
-# [tool.databricks.environment]
-# environment_version = "5"
-# ///
-# MAGIC %sql
-# MAGIC SELECT*
-# MAGIC FROM retail.sales.retail_sales_dataset
